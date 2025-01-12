245f772c9d7053d559e7706de528efb9422cff17
morozvitali <morozvitali99@gmail.com>
2025-01-12 15:50:36 +0200

    Task 04 Inversion Of Control

---
+++ src/main/java/spring_introduction/Test.java
@@ -0,0 +1,11 @@
+package spring_introduction;
+
+public class Test {
+    public static void main(String[] args) {
+        Talker talker1 = new Dog();
+        talker1.say();
+
+        Talker talker2 = new Cat();
+        talker2.say();
+    }
+}
