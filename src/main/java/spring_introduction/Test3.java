4cc3b88ac636331e6a966a8f0a0494cac76a40e7
morozvitali <morozvitali99@gmail.com>
2025-01-14 11:19:00 +0200

    Task 09 Dependency Injection Using class parameters

--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -1,1 +1,16 @@
 package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Person person = context.getBean("myPerson", Person.class);
+        person.callYourPet();
+        System.out.println("person age = " + person.getAge() + " name = " + person.getName() + ", surname = " + person.getSurname());
+        context.close();
+    }
+}
