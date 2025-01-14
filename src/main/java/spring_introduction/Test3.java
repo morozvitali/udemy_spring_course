84494f2de9e6778c1321d0380788417a75d8737b
morozvitali <morozvitali99@gmail.com>
2025-01-14 10:43:08 +0200

    Task 08 Dependency Injection Using Setters

--- src/main/java/spring_introduction/Test3.java
+++ src/main/java/spring_introduction/Test3.java
@@ -1,16 +1,1 @@
 package spring_introduction;
-
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-public class Test3 {
-
-    public static void main(String[] args) {
-
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        //Talker talker = context.getBean("myTalker", Talker.class);
-
-        Person person = context.getBean("myPerson", Person.class);
-        person.callYourPet();
-        context.close();
-    }
-}
