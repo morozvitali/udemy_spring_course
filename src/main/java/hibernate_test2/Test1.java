ad007c3186342e1d81dbca251c434c9051d4f8a2
morozvitali <morozvitali99@gmail.com>
2025-01-29 12:54:47 +0200

    Task 40 hibernate one to one realisation

--- src/main/java/hibernate_test2/Test1.java
+++ src/main/java/hibernate_test2/Test1.java
@@ -1,24 +1,32 @@
-package hibernate_test;
+package hibernate_test2;
 
-import hibernate_test.entity.Employee;
+import hibernate_test2.entity.Employee;
+import hibernate_test2.entity.Detail;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class Test1 {
-
     public static void main(String[] args) {
+        /*     TODO основнаий код який не треба видаляти       */
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
                 .buildSessionFactory();
         try {
             Session session = factory.getCurrentSession();
-            Employee employee = new Employee
-                    ("Rocky", "Balboa", "boxing", 30000);
+        /*     TODO основнаий код який не треба видаляти       */
+
+            Employee employee = new Employee("Bill", "Murray", "cinema", 658468);
+            Detail detail = new Detail("Los Angeles", "1651", "murray@gmail.com");
+            employee.setDetail(detail);
+
             session.beginTransaction();
             session.save(employee);
+
             session.getTransaction().commit();
+            System.out.println("+");
         } finally {
             factory.close();
         }
